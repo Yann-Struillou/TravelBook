ccf76c7e4c3c088c9c4b6680467b0dc530eda049
Yann Struillou <98101863+ystruillou@users.noreply.github.com>
2025-12-19 01:50:16 +0100

    Update StartApplication.sh

--- TravelBook/StartApplication.sh
+++ TravelBook/StartApplication.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e
+
+# Vérification que les secrets existent
+for secret in /run/secrets/travelbook_azure_client_id /run/secrets/travelbook_azure_client_secret /run/secrets/travelbook_azure_tenant_id /run/secrets/travelbook_keyvault_uri; do
+  [ -f "$secret" ] || { echo "Secret $secret missing"; exit 1; }
+done
+
+# Exporter les secrets en variables d'environnement
 export AZURE_TENANT_ID=$(cat /run/secrets/travelbook_azure_tenant_id)
 export AZURE_CLIENT_ID=$(cat /run/secrets/travelbook_azure_client_id)
 export AZURE_CLIENT_SECRET=$(cat /run/secrets/travelbook_azure_client_secret)
 export KEYVAULT_URI=$(cat /run/secrets/travelbook_keyvault_uri)
+
+# Démarrer l'application Blazor
 exec dotnet TravelBook.dll
