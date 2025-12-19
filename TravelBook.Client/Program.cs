e24041a6f4c25dc6fd49fa5af0860cfc8c0b4c49
Yann Struillou <98101863+ystruillou@users.noreply.github.com>
2025-12-19 09:42:31 +0100

    Update Program.cs

--- TravelBook.Client/Program.cs
+++ TravelBook.Client/Program.cs
@@ -3,11 +3,6 @@ using TravelBook.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddAuthorizationCore();
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddAuthenticationStateDeserialization();
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}") });
-builder.Services.AddScoped<UsersService>();
 
 await builder.Build().RunAsync();
