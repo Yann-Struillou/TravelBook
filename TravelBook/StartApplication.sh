5aa2e7015e552ac2006cf547d3606013ac721f43
Yann Struillou <98101863+ystruillou@users.noreply.github.com>
2025-12-19 05:24:53 +0100

    Update StartApplication.sh

--- TravelBook/StartApplication.sh
+++ TravelBook/StartApplication.sh
@@ -6,7 +6,7 @@ for secret_file in \
     "$AZURE_CLIENT_ID_FILE" \
     "$AZURE_CLIENT_SECRET_FILE" \
     "$AZURE_TENANT_ID_FILE" \
-    "$KEYVAULT_URI_FILE"; do
+    "$AZURE_KEYVAULT_URI_FILE"; do
   [ -f "$secret_file" ] || { echo "Secret file $secret_file missing"; exit 1; }
 done
 
@@ -14,7 +14,7 @@ done
 export AZURE_CLIENT_ID=$(cat "$AZURE_CLIENT_ID_FILE")
 export AZURE_CLIENT_SECRET=$(cat "$AZURE_CLIENT_SECRET_FILE")
 export AZURE_TENANT_ID=$(cat "$AZURE_TENANT_ID_FILE")
-export KEYVAULT_URI=$(cat "$KEYVAULT_URI_FILE")
+export AZURE_KEYVAULT_URI=$(cat "$AZURE_KEYVAULT_URI_FILE")
 
 # Lancer l'application
 exec dotnet TravelBook.dll
